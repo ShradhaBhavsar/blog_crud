d195d827e963ce2fec60b21d26050162164b6bdd
shradhabhavsar <shradhabhavsar8@gmail.com>
2023-01-24 16:31:28 +0530

    Feature: Edit, Show And Destroy

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,6 +17,31 @@ class BlogsController < ApplicationController
     end
   end
 
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def blog_params
       params.require(:blog).permit(:name, :description)
